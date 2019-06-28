cd241ce32a8dc01fabdccffd82bff2b503fc48ed
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-29 02:52:35 +0600

    Add __str__ method for both file and folder model, add full_path and extension method to file model

--- file_manager/file/models.py
+++ file_manager/file/models.py
@@ -9,3 +9,14 @@ class File(models.Model):
     path = models.CharField(max_length=128, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    @property
+    def full_path(self):
+        return f'{self.folder}::{self.name}'
+
+    @property
+    def extension(self):
+        return str(self.name).split('.')[-1]
--- file_manager/folder/models.py
+++ file_manager/folder/models.py
@@ -4,5 +4,12 @@ from django.db import models
 class Folder(models.Model):
     name = models.CharField(max_length=128)
     text_color = models.CharField(max_length=32, default='#000000', blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+    def __str__(self):
+        full_name = f'{self.name}'
+        if self.parent:
+            full_name = f'{self.parent}::' + full_name
+        return full_name
