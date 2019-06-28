3018479c4536ec8001ecdb7c64cf621f02fa689b
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-29 01:18:11 +0600

    Add folder and file url to main urls

--- file_manager/urls.py
+++ file_manager/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('folder/', include('file_manager.folder.urls', namespace='folder')),
+    path('file/', include('file_manager.file.urls', namespace='file')),
 ]
