bf9a81c2c4965d611e81d55b33aa496880d4f7d5
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-29 02:10:56 +0600

    Add model, add.html and list.html for file app, add text_color field to folder model

--- file_manager/file/models.py
+++ file_manager/file/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from file_manager.folder.models import Folder
+
+
+class File(models.Model):
+    name = models.CharField(max_length=128)
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    path = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
--- file_manager/folder/models.py
+++ file_manager/folder/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 class Folder(models.Model):
     name = models.CharField(max_length=128)
+    text_color = models.CharField(max_length=32, default='#000000', blank=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
