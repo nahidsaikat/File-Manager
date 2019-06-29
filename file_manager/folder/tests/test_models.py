31f5f128dcd1e2f705af7353d4a54a37a48ff5b2
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-29 16:11:10 +0600

    Add test_folder_str test and some refactoring

--- file_manager/folder/tests/test_models.py
+++ file_manager/folder/tests/test_models.py
@@ -7,11 +7,24 @@ from file_manager.folder.tests.factory import FolderFactory
 fake = Faker()
 
 
-class TestFolder:
+class BaseFixture:
 
-    @pytest.mark.django_db
-    def test_folder_create(self):
-        FolderFactory(name=fake.name())
-        FolderFactory(name=fake.name())
+    @pytest.fixture
+    def folder_1(self, db):
+        return FolderFactory(name=fake.name())
 
+    @pytest.fixture
+    def folder_2(self, db):
+        return FolderFactory(name=fake.name())
+
+
+class TestFolder(BaseFixture):
+
+    def test_folder_create(self, folder_1, folder_2):
         assert Folder.objects.count() == 2
+
+    def test_folder_str(self, folder_1, folder_2):
+        folder_2.parent = folder_1
+        folder_2.save()
+
+        assert str(folder_2) == f'{folder_1.name}::{folder_2.name}'
