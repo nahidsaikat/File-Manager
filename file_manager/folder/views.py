00baa617ff067ec1a20bf1710a07f28cf5f45e8f
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-30 00:19:27 +0600

    Pass file_list as context data in folder list view

--- file_manager/folder/views.py
+++ file_manager/folder/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, CreateView
 
 from file_manager.folder.forms import FolderForm
 from file_manager.folder.models import Folder
+from file_manager.file.models import File
 
 
 class FolderCreateView(CreateView):
@@ -14,4 +15,23 @@ class FolderListView(ListView):
     template_name = 'folder/list.html'
 
     def get_queryset(self):
-        return Folder.objects.filter(parent=None).all()
+        pk = self.request.GET.get('id')
+        queryset = Folder.objects.all()
+        if pk:
+            queryset = queryset.filter(parent__pk=pk)
+        else:
+            queryset = queryset.filter(parent=None)
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+
+        pk = self.request.GET.get('id')
+        file_query = File.objects.all()
+        if pk:
+            file_query = file_query.filter(folder__pk=pk)
+        else:
+            file_query = file_query.filter(folder=None)
+        context['file_list'] = file_query
+
+        return context
